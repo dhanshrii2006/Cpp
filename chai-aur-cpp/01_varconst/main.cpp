51891ee4f74956247f0178e4f196f6a0092aa4d8
neeooonn <dhanshriwanjari4009@gmail.com>
2025-07-03 15:43:06 +0530

    Update main.cpp

--- chai-aur-cpp/01_varconst/main.cpp
+++ chai-aur-cpp/01_varconst/main.cpp
@@ -1,25 +1,18 @@
 
 #include <iostream>
-
 using namespace std;
-
 int main()
 {
    int score;
     score = 110;
 
-    const int uid = 232323; // constant mtlb can't change futher
-
-    int hiteshBalance = 500;
-    hiteshBalance = 1000;
-
-    // uid = 1223;
+    const int uid = 232323;   
+   // constant mtlb can't change futher
     cout << "Welcome to chai with cpp 1" << endl ;
-    cout << "Welcome to chai with cpp 2" << endl ;
-    cout << "Welcome to chai with cpp 3" << endl ;
-
+   
     return 0;
 }
 
 // using control slash select many lines then use control slash 
 /* is also method*/
+
